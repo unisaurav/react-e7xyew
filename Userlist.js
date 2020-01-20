c4417d027be0d42bc4ca202e68b7581deccfc236
unisaurav <unisaurav@gmail.com>
2020-01-20 19:59:00 +0530

    1 file modified

--- Userlist.js
+++ Userlist.js
@@ -2,21 +2,39 @@ import React from 'react'
 
 class Userlist extends React.Component{
   componentWillMount(){
-    console.log(this.props.location.state)
+    console.log(this.props.location.state.userdata)
+   this.userlistitr();
   }
 
   userlistitr(){
-    var a;
-    this.state.location.state.map((data,index)=>{
-      <div>{data.username}</div>  
-    })
+    var data=[];
+    var curuser=[];
+    for(var i=0;i<this.props.location.state.userdata.length;i++){
+   
+        if(this.props.location.state.userdata[i].LoginStatus==false){
+          data.push(this.props.location.state.userdata[i]);
+        }else{
+          curuser.push(this.props.location.state.userdata[i]);
+        };
+      
+    }
+    console.log(data);
+
+    // var a;
+    // this.state.location.state.map((data,index)=>{
+    //   <div>{data.username}</div>  
+    // })
   }
 
 render(){
   return(
 
     <div>
-
+    {this.props.location.state.userdata.map((data,index)=>
+    <div>
+    <p>{data.Username}</p>
+    <button> view</button></div>)}
+   
     </div>
   );
 }
